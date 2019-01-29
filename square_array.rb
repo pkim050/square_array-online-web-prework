7596a24ec8f3bee1abcf1c05b7d1ff8245466c1e
Patrick Kim <pkim050@gmail.com>
2019-01-29 11:43:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   array.each do |number|
     puts "Before math #{number}."
-    number **= number
+    number **= 2
     puts "After math #{number}."
   end
 end
