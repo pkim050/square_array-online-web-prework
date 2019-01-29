e2afc9b1e7704c122d2349b3cf8b9db044cabefe
Patrick Kim <pkim050@gmail.com>
2019-01-29 11:52:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,6 @@
 def square_array(array)
   # your code here
   array.each do |number|
-    puts "Before math #{number}."
-    number **= 2
-    puts "After math #{number}."
-  end
-  array.each do |number|
-    puts "Result: #{number}."
+    array.map! {|number **= 2
   end
 end
